07cc0abcacb640ec841a0729716d77d16b8c5a30
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-02-08 22:52:02 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+    'config.lua',
+    'client/tats.lua',
+    'client/client.lua',
+}
+server_scripts {
+    'config.lua',
+    'server/server.lua',
+} 
+ui_page 'html/index.html'
+
+files {
+    'html/*',
+    'html/assets/*',
+}
+
+lua54 'yes'
